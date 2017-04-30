b7da2534ef6a08ca94cc881f7fbfa0f2398b38a5
celestian <petr.celestian@gmail.com>
2017-04-30 21:40:10 +0200

    Converting into daemon

--- src/listener/listener.c
+++ src/listener/listener.c
@@ -1,10 +1,13 @@
+#include <fcntl.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <limits.h>
-#include <stdint.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <syslog.h>
 #include <unistd.h>
 
 #include "src/nbus/nbus.h"
@@ -12,7 +15,7 @@
 #include "src/utils/data.h"
 #include "src/utils/errors.h"
 
-int main(void)
+int main_module(void)
 {
     TALLOC_CTX* mem_ctx;
     struct url_conn_ctx* url_conn_ctx;
@@ -72,49 +75,46 @@ int main(void)
     exit(EXIT_SUCCESS);
 }
 
-/*
-int main(int argc, char* argv[]) {
-    FILE *fp = NULL;
-    pid_t process_id = 0;
+int main(int argc, char* argv[])
+{
+    pid_t pid = 0;
     pid_t sid = 0;
-    // Create child process
-    process_id = fork();
-    // Indication of fork() failure
-    if (process_id < 0) {
-        printf("fork failed!\n");
-        // Return failure in exit status
-        exit(1);
+    int ret;
+    bool next_iteration = true;
+
+    pid = fork();
+    if (pid < 0) {
+        printf("[error] Listener didn't start [fork()].\n");
+        exit(EXIT_FAILURE);
     }
-    // PARENT PROCESS. Need to kill it.
-    if (process_id > 0) {
-        printf("process_id of child process %d \n", process_id);
-        // return success in exit status
+    if (pid > 0) {
+        printf("[success] Listener started.\n");
         exit(EXIT_SUCCESS);
     }
-    //unmask the file mode
+
     umask(0);
-    //set new session
+
     sid = setsid();
     if (sid < 0) {
-        // Return failure
-        exit(1);
+        printf("[error] Listener didn't start [setsid()].\n");
+        exit(EXIT_FAILURE);
     }
-    // Change the current working directory to root.
-    chdir("/");
-    // Close stdin. stdout and stderr
+
+    ret = chdir("/tmp");
+    if (ret < 0) {
+        printf("[error] Listener didn't start [chdir() [%i] [%s]].\n", ret,
+               strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
-    // Open a log file in write mode.
-    fp = fopen("Log.txt", "w+");
-    while (1) {
-        //Dont block context switches, let the process sleep for some time
-        sleep(1);
-        fprintf(fp, "Logging info...\n");
-        fflush(fp);
-        // Implement and call some function that does core work for this daemon.
+
+    while (next_iteration) {
+        main_module();
+        next_iteration = false;
     }
-    fclose(fp);
+
     exit(EXIT_SUCCESS);
 }
- */
