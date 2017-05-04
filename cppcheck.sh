53dc744378180a07f57af4ef26a333833c487677
celestian <petr.celestian@gmail.com>
2017-05-04 13:50:31 +0200

    Add cppcheck script for static code analysis

---
+++ cppcheck.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+RELATIVE_FLIP_SOURCE_PATH=`dirname ${BASH_SOURCE[@]}`
+FLIP_SOURCE_PATH=`realpath ${RELATIVE_FLIP_SOURCE_PATH}`
+
+cppcheck \
+    --language=c \
+    --std=c11 \
+    --enable=all \
+    --suppress=missingIncludeSystem \
+    --check-config \
+    -I${FLIP_SOURCE_PATH}/src/../ \
+    -I${FLIP_SOURCE_PATH}/$(uname -m) \
+    ${FLIP_SOURCE_PATH}/src
